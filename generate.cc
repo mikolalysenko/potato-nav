42fb424a76432e298f28b4df3106a84d361625ec
mikolalysenko <mikolalysenko@gmail.com>
2016-02-05 19:42:59 -0800

    add some graph generators for testing

--- generate.cc
+++ generate.cc
@@ -1,18 +1,44 @@
 #include <cstdint>
 
+#include "graph.h"
 #include "csr.h"
 #include "generate.h"
 
 using namespace SPUD;
 using namespace Generate;
 
-CSRGraph* random(int64_t numVerts, int64_t numArcs) {
-
+CSRGraph* random(const char* path, int64_t numVerts, int64_t numArcs) {
+  const arcs = new Arc[numArcs];
+  for(int64_t i=0; i<numArcs; ++i) {
+    arcs[i].source = random() % numVerts;
+    arcs[i].target = random() % numVerts;
+    arcs[i].cost = drand48();
+  }
+  auto result = CSRGraph::fromArcList(path, numVerts, numArcs, arcs);
+  delete[] arcs;
+  return result;
 }
 
 CSRGraph* ring(int64_t numVerts) {
-  auto result = CSRGraph
+  const arcs = new Arc[numVerts];
+  for(int64_t i=0; i<numVerts; ++i) {
+    arcs[i].source = i;
+    arcs[i].target = (i + 1) % numVerts;
+    arcs[i].cost = 1.0;
+  }
+  auto result = CSRGraph::fromArcList(path, numVerts, numVerts, arcs);
+  delete[] arcs;
+  return result;
 }
 
 CSRGraph* hub(int64_t numVerts) {
+  const arcs = new Arc[numVerts];
+  for(int64_t i=1; i<numVerts; ++i) {
+    arcs[i].source = 0;
+    arcs[i].target = i;
+    arcs[i].cost = 1.0;
+  }
+  auto result = CSRGraph::fromArcList(path, numVerts, numArcs, arcs);
+  delete[] arcs;
+  return result;
 }
--- generate.h
+++ generate.h
@@ -5,8 +5,8 @@
 
 namespace SPUD {
   namespace Generate {
-    CSRGraph* random(int64_t numVerts, int64_t numArcs);
-    CSRGraph* ring(int64_t numVerts);
-    CSRGraph* hub(int64_t numVerts);
+    CSRGraph* random(const char*, int64_t numVerts, int64_t numArcs);
+    CSRGraph* ring(const char*, int64_t numVerts);
+    CSRGraph* hub(const char*, int64_t numVerts);
   }
 }
